653f2f9e37ea0223b9a63b80720216a4b368f1da
Jin G <gao-jin@outlook.com>
2023-10-26 10:13:33 -0400

    changing plazaview adding options

--- client/views/PlazaView.vue
+++ client/views/PlazaView.vue
@@ -8,6 +8,9 @@ import { onMounted } from "vue";
 const { setPageTitle } = useTitleStore();
 const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
 
+let selectedCategory = { value: "Blogs" };
+let selectedFilter = { value: "Latest" };
+
 onMounted(() => {
   setPageTitle("PlazaPlaza");
 });
@@ -16,12 +19,20 @@ onMounted(() => {
 <template>
   <main>
     <h1>Plaza</h1>
-    <section>
-      <!-- <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
-      <h1 v-else>Please login!</h1> -->
-    </section>
     <AbstractWorldmapComponent />
+    <div class="button-group">
+      <button v-for="(btn, index) in ['Blogs', 'Q&A', 'Wiki']" :key="index" :class="{ active: selectedCategory.value === btn }" @click="selectedCategory.value = btn">
+        {{ btn }}
+      </button>
+    </div>
 
+    <div class="filter-group">
+      <button v-for="(filter, index) in ['Latest', 'Trending', 'Nearby']" :key="index" :class="{ active: selectedFilter.value === filter }" @click="selectedFilter.value = filter">
+        {{ filter }}
+      </button>
+    </div>
+
+    <!-- <PostListComponent :category="selectedCategory.value" :filter="selectedFilter.value" /> -->
     <PostListComponent />
   </main>
 </template>
@@ -29,5 +40,25 @@ onMounted(() => {
 <style scoped>
 h1 {
   text-align: center;
+}
+.button-group,
+.filter-group {
+  display: flex;
+  justify-content: center;
+  margin: 1rem 0;
+}
+
+button {
+  padding: 0.5rem 1rem;
+  margin: 0.5rem;
+  border: none;
+  background-color: #f7f7f7;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+button.active {
+  background-color: #e0e0e0;
 }
 </style>
