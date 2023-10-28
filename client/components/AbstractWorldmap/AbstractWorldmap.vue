9aaaec2eaede451718916fab4310ea13af9f4ab1
Jin G <gao-jin@outlook.com>
2023-10-27 20:52:28 -0400

    update abstract worldmap

--- client/components/AbstractWorldmap/AbstractWorldmap.vue
+++ client/components/AbstractWorldmap/AbstractWorldmap.vue
@@ -2,42 +2,16 @@
 // here is js, will fix to ts
 import { ref } from "vue";
 import * as d3 from "d3";
-import * as topojson from "topojson-client";
-import us from "@/assets/us.json";
+import { onMounted } from "vue";
+import data from "@/assets/data.json";
+// console.log("us", us);
 
 export default {
   name: "HorizontalBar",
   props: {
     entries: {
       type: Array,
-      default: () => [
-        { name: "E", value: 0.12702 },
-        { name: "T", value: 0.09056 },
-        { name: "A", value: 0.08167 },
-        { name: "O", value: 0.07507 },
-        { name: "I", value: 0.06966 },
-        { name: "N", value: 0.06749 },
-        { name: "S", value: 0.06327 },
-        { name: "H", value: 0.06094 },
-        { name: "R", value: 0.05987 },
-        { name: "D", value: 0.04253 },
-        { name: "L", value: 0.04025 },
-        { name: "C", value: 0.02782 },
-        { name: "U", value: 0.02758 },
-        { name: "M", value: 0.02406 },
-        { name: "W", value: 0.0236 },
-        { name: "F", value: 0.02288 },
-        { name: "G", value: 0.02015 },
-        { name: "Y", value: 0.01974 },
-        { name: "P", value: 0.01929 },
-        { name: "B", value: 0.01492 },
-        { name: "V", value: 0.00978 },
-        { name: "K", value: 0.00772 },
-        { name: "J", value: 0.00153 },
-        { name: "X", value: 0.0015 },
-        { name: "Q", value: 0.00095 },
-        { name: "Z", value: 0.00074 },
-      ],
+      default: () => [],
     },
     columns: {
       type: Array,
@@ -52,73 +26,152 @@ export default {
   setup(props) {
     const svgRef = ref(null);
 
-    const svg = d3.select(svgRef.value);
+    onMounted(() => {
+      let svg = d3.select(svgRef.value);
+      const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
+      const width = 900;
+      const height = 600;
+      const margin = 1;
 
-    const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
-
-    // Define width, height, and other D3 related constants
-    const width = 975;
-    const height = 610;
-
-    svg.attr("viewBox", [0, 0, width, height]).attr("width", width).attr("height", height).attr("style", "max-width: 100%; height: auto;").on("click", reset);
-
-    const path = d3.geoPath();
-
-    const g = svg.append("g");
-
-    const states = g
-      .append("g")
-      .attr("fill", "#444")
-      .attr("cursor", "pointer")
-      .selectAll("path")
-      .data(topojson.feature(us, us.objects.states).features)
-      .join("path")
-      .on("click", clicked)
-      .attr("d", path);
-
-    states.append("title").text((d) => d.properties.name);
-
-    g.append("path")
-      .attr("fill", "none")
-      .attr("stroke", "white")
-      .attr("stroke-linejoin", "round")
-      .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
-
-    svg.call(zoom);
-
-    function reset() {
-      states.transition().style("fill", null);
+      // Set the attributes directly on this SVG, rather than creating a new one
       svg
-        .transition()
-        .duration(750)
-        .call(zoom.transform, d3.zoomIdentity, d3.zoomTransform(svg.node()).invert([width / 2, height / 2]));
-    }
+        .attr("viewBox", [-margin, -margin, width, height])
+        .attr("width", width)
+        .attr("height", height)
+        .attr("style", "max-width: 100%; height: auto; font: 6px sans-serif;")
+        .on("click", reset)
+        .call(zoom);
 
-    function clicked(event, d) {
-      const [[x0, y0], [x1, y1]] = path.bounds(d);
-      event.stopPropagation();
-      states.transition().style("fill", null);
-      d3.select(this).transition().style("fill", "red");
-      svg
-        .transition()
-        .duration(750)
-        .call(
-          zoom.transform,
-          d3.zoomIdentity
-            .translate(width / 2, height / 2)
-            .scale(Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)))
-            .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-          d3.pointer(event, svg.node()),
-        );
-    }
+      // Create a root 'g' container inside the SVG
+      const gRoot = svg.append("g");
 
-    function zoomed(event) {
-      const { transform } = event;
-      g.attr("transform", transform);
-      g.attr("stroke-width", 1 / transform.k);
-    }
+      const name = (d) => d.id.split(".").pop(); // "Strings" of "flare.util.Strings"
+      const group = (d) => d.id.split(".")[1]; // "util" of "flare.util.Strings"
+      const names = (d) => name(d).split(/(?=[A-Z][a-z])|\s+/g); // ["Legend", "Item"] of "flare.vis.legend.LegendItems"
 
-    return svg.node();
+      // Specify the number format for values.
+      const format = d3.format(",d");
+
+      // Convert lat/lng to x/y using Mercator projection
+      const projection = d3.geoMercator().fitSize([width, height], { type: "FeatureCollection", features: data });
+
+      // Categorical color scale
+      const color = d3.scaleOrdinal(d3.schemeTableau10);
+
+      // Create the pack layout.
+      const pack = d3
+        .pack()
+        .size([width - margin * 2, height - margin * 2])
+        .padding(3);
+
+      // Compute the hierarchy from the (flat) data; expose the values
+      // for each node; lastly apply the pack layout.
+      const root = pack(d3.hierarchy({ children: data }).sum((d) => d.value));
+
+      // Place each (leaf) node according to the layout’s x and y values.
+      // Update this in the pack layout
+      const node = gRoot
+        .selectAll()
+        .data(root.leaves())
+        .join("g")
+        .attr("transform", (d) => `translate(${d.x},${d.y})`)
+        .on("click", clicked);
+
+      const simulation = d3
+        .forceSimulation(data)
+        .force("x", d3.forceX((d) => projection([d.lng, d.lat])[0]).strength(0.2))
+        .force("y", d3.forceY((d) => projection([d.lng, d.lat])[1]).strength(0.2))
+        .force(
+          "collide",
+          d3.forceCollide((d) => d.r + 2),
+        ) // +2 to ensure a little spacing
+        .stop();
+
+      for (let i = 0; i < 120; i++) simulation.tick();
+
+      // Add a title.
+      node.append("title").text((d) => `${d.data.id}\n${format(d.value)}`);
+
+      // draw hexagon
+      node
+        .append("path")
+        .attr("d", (d) => hexagon(0, 0, d.r)) // Use d.r as the radius for the hexagon.
+        .attr("fill-opacity", 0.7)
+        .attr("fill", (d) => color(group(d.data)))
+        .on("click", clicked);
+
+      // Add a label.
+      const text = node.append("text").attr("clip-path", (d) => `circle(${d.r})`);
+
+      // Add a tspan for each CamelCase-separated word.
+      text
+        .selectAll()
+        .data((d) => names(d.data))
+        .join("tspan")
+        .attr("x", 0)
+        .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.35}em`)
+        .text((d) => d)
+        .attr("text-anchor", "middle")
+        .attr("dy", "0.35em");
+
+      // Add a tspan for the node’s value.
+      text
+        .append("tspan")
+        .attr("x", 0)
+        .attr("y", (d) => `${names(d.data).length / 2 + 0.35}em`)
+        // .attr("fill-opacity", 0.7)
+        .text((d) => format(d.value))
+        .attr("text-anchor", "middle")
+        .attr("dy", "0.35em");
+
+      return Object.assign(svg.node(), { scales: { color } });
+
+      function reset() {
+        node.transition().style("fill", (d) => color(group(d.data)));
+        svg
+          .transition()
+          .duration(750)
+          .call(zoom.transform, d3.zoomIdentity, d3.zoomTransform(svg.node()).invert([width / 2, height / 2]));
+      }
+
+      function clicked(event, d) {
+        const bbox = d3.select(this).node().getBBox();
+        const [x0, y0, x1, y1] = [bbox.x, bbox.y, bbox.x + bbox.width, bbox.y + bbox.height];
+        event.stopPropagation();
+
+        // node.transition().style("fill", (d) => color(white));
+        // d3.select(this).transition().style("fill", "red");
+
+        const MAX_ZOOM_SCALE = 3; // You can tweak this value
+        const scale = Math.min(MAX_ZOOM_SCALE, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height));
+
+        const translateX = width / 2 - scale * d.x;
+        const translateY = height / 2 - scale * d.y;
+
+        svg.transition().duration(750).call(zoom.transform, d3.zoomIdentity.translate(translateX, translateY).scale(scale), d3.pointer(event, svg.node()));
+      }
+
+      function zoomed(event) {
+        const { transform } = event;
+        gRoot.attr("transform", transform);
+      }
+
+      function hexagon(x, y, radius) {
+        const angleStep = Math.PI / 3;
+        let path = d3.path();
+        path.moveTo(x + radius, y);
+        for (let i = 1; i < 6; i++) {
+          let angle = angleStep * i;
+          let relX = x + radius * Math.cos(angle);
+          let relY = y + radius * Math.sin(angle);
+          path.lineTo(relX, relY);
+        }
+        path.closePath();
+        return path.toString();
+      }
+    });
+    console.log("last svgRef", svgRef);
+    return { svgRef }; // Return the reactive reference
   },
 };
 </script>
@@ -126,27 +179,11 @@ export default {
 <template>
   <div>
     <svg ref="svgRef"></svg>
-
-    <!-- <svg :viewBox="`0 0 ${width} ${height}`" xmlns="http://www.w3.org/2000/svg">
-      <g v-for="(entry, index) in entries" :key="'bar-' + index" fill="seagreen">
-        <rect :x="x(0)" :y="y(index)" :width="Number(x(entry.value)) - Number(x(0))" :height="y.bandwidth()" />
-      </g>
-      <g v-for="(entry, index) in entries" :key="'text-' + index" fill="white">
-        <text :x="x(entry.value)" :y="Number(y(index)) + Number(y.bandwidth()) / 2" dx="-45" dy="0.35em">{{ formattedText(entry.value) }}</text>
-      </g>
-      <g :transform="`translate(0, ${margin.top})`">
-        <g v-for="(num, index) in maxNumber" :key="'x-' + num" opacity="1" :transform="`translate(${Number(x(index)) / 101 + 45}, 0)`">
-          <line stroke="darkgreen" y2="-6"></line>
-          <text fill="darkgreen" x="-4" y="-9" dy="0em">{{ index + format }}</text>
-        </g>
-      </g>
-      <g :transform="`translate(${margin.left}, 0)`">
-        <path class="domain" stroke="darkgreen" d="M0.5,30.5V683.5"></path>
-        <g v-for="(entry, index) in entries" :key="'y-' + index" opacity="1" :transform="`translate(0, ${Number(y(index)) + 12})`">
-          <line stroke="darkgreen" x2="-6"></line>
-          <text fill="darkgreen" x="-25" dy="0.32em">{{ entry.name }}</text>
-        </g>
-      </g>
-    </svg> -->
   </div>
 </template>
+
+<style scoped>
+svg {
+  border: 1px solid black;
+}
+</style>
